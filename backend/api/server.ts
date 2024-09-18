5486fd08f244a847723596ee98d34c61a29a72c0
stapp425 <stapp425@gmail.com>
2024-09-17 21:40:37 -0500

    vercel pls autodetect api

--- backend/api/server.ts
+++ backend/api/server.ts
@@ -1,7 +1,7 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
-import apiRouter from "./api/api"
+import apiRouter from "./routes/api"
 
 dotenv.config()
 const app = express()
